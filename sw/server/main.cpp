1eeed3fa32a761a7a7d6dd927ca21460653572d9
Alexey Shchurov <alexiskhb@gmail.com>
2017-10-28 10:34:21 +1000

    Add trivial ajax data exchange

--- sw/server/main.cpp
+++ sw/server/main.cpp
@@ -1,36 +1,47 @@
 #include <iostream>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <fastcgi++/request.hpp>
-#include <fastcgi++/manager.hpp>
+#include <vector>
+#include <random>
 
 #include "../driver/ioctl_commands.h"
 
-#define DEVICE_NAME                 "ham"
+#include <fastcgi++/request.hpp>
+#include <fastcgi++/manager.hpp>
+
+std::random_device rd;
 
 class Modem: public Fastcgipp::Request<wchar_t>
 {
 public:
-    Modem() : Fastcgipp::Request<wchar_t>(1024) 
+    Modem() : Fastcgipp::Request<wchar_t>(5*1024) 
     {}
 private:
     enum class Event {
-        OFF_CLICKED = 0,
-        ON_CLICKED = 1
     };
 
     bool response()
     {
+        using Fastcgipp::Encoding;
 
+        out <<  L"Content-Type: text/html\n\n";
+        out <<  std::to_wstring(rd());
+        // out <<  L"<form method=\"post\"> <input type=\"text\" name=\"abc\"> <input type=\"submit\"></form>";
+        // for (const auto& post: environment().posts) {
+            // out << L"<b>" << Encoding::HTML << post.first << Encoding::NONE
+                // << L":</b>" << Encoding::HTML << post.second
+                // << Encoding::NONE << L"<br/>";
+            // ioctl();
+        // }
         return true;
     }
 };
 
-int main() 
+int main(int argc, char** argv)
 {
     Fastcgipp::Manager<Modem> manager;
     manager.setupSignals();
-    manager.listen();
+    manager.listen("127.0.0.1", argc > 1 ? argv[1] : "8000");
     manager.start();
     manager.join();
 }
--- sw/server/www/js/events.js
+++ sw/server/www/js/events.js
@@ -1,14 +1,50 @@
-const Event = {
-    OFF_CLICKED: 0,
-    ON_CLICKED:  1
-}
-
 $(document).ready(function() {
-    $("#on_button").click(function() {
-        alert("ON")
+    // fetch('http://localhost/sv').then(function(response) {
+    //     response.text().then(function(text) {
+    //         document.getElementById('result').innerHTML = text;
+    //     });
+    // });
+
+    $("#set_mask_button").click(function() {
+        $.ajax({
+            url: "http://localhost/sv",
+            type: "GET",
+            // data: $("#set_mask_button"),
+            success: function (data) {
+                document.getElementById('result2').innerHTML = data;
+            }
+        });
     });
 
-    $("#off_button").click(function() {
-        alert("OFF")
+    var interval = 0;
+    function start() {
+        setTimeout( function() {
+            update_contents();
+            interval = 1;
+            setInterval( function() {
+                update_contents();
+            }, interval*1000);
+        }, interval*1000);    
+    }
+
+    function update_contents() {
+        $.ajax({
+            url: "http://localhost/sv",
+            type: "GET",
+            success: function(result) {
+                // $('.time').html(result);
+                document.getElementById('result').innerHTML = result;
+            }
+        }); 
+    }
+
+    $(window).load(function(){
+        // var time = new Date();
+        // interval = 1 - time.getSeconds();
+        // if(interval==1)
+        //     interval    =   0;
+        start();
     });
 });
+
+
