1190d0e5f80bd1daef0d69fe861c9b27d8de04b8
Nikita Utin <utinnikitas@gmail.com>
2017-11-22 01:31:31 +1000

    Add dma test driver

--- sw/driver/ham_drv.c
+++ sw/driver/ham_drv.c
@@ -5,16 +5,28 @@
 #include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
 #include <asm/uaccess.h>
 #include "ioctl_commands.h"
 
-#define DEVICE_NAME                 "ham"
-#define UINPUT_BASE                 0xff200000
-#define UINPUT_SIZE                 0x00070000
-#define INTERRUPT_NUM               77
+#define DEVICE_NAME                    "ham"
+#define UINPUT_BASE                    0xff200000
+#define UINPUT_SIZE                    0x00070000
+#define INTERRUPT_NUM                  72
 
-#define MODULE_OFFSET               (ham_drv_mem + 0x00050000)
-#define GET_ADC_OFFSET              (MODULE_OFFSET + 1 * sizeof(u32))
+
+#define MODULE_OFFSET                  (ham_drv_mem + 0x00050000)
+#define GET_ADC_OFFSET                 (MODULE_OFFSET + 1 * sizeof(u32))
+
+#define DMA_MODULE_OFFSET              (ham_drv_mem + 0x00000000)
+#define DMA_STATUS_REG_OFFSET          (DMA_MODULE_OFFSET)
+#define DMA_READADDRESS_REG_OFFSET     (DMA_MODULE_OFFSET + 4)
+#define DMA_WRITEADDRESS_REG_OFFSET    (DMA_MODULE_OFFSET + 8)
+#define DMA_LENGTH_REG_OFFSET          (DMA_MODULE_OFFSET + 12)
+#define DMA_CONTROL_REG_OFFSET         (DMA_MODULE_OFFSET + 24)
+
+#define DMA_BUFFER_SIZE 4 
+
 
 static void *ham_drv_mem = NULL;
 static struct class *ham_drv_class  = NULL;
@@ -23,6 +35,23 @@ static struct device *ham_drv_device = NULL;
 static int major_number;
 
 static int adc_value = 0;
+static void *buffer;
+
+static void ham_drv_dma_print_registers(void)
+{
+    printk(KERN_INFO "ham_drv: dma reg status           %u\n", ioread32(DMA_STATUS_REG_OFFSET));
+    printk(KERN_INFO "ham_drv: dma reg readaddress      %X\n", ioread32(DMA_READADDRESS_REG_OFFSET));
+    printk(KERN_INFO "ham_drv: dma reg writeaddress     %X\n", ioread32(DMA_WRITEADDRESS_REG_OFFSET));
+    printk(KERN_INFO "ham_drv: dma reg length           %u\n", ioread32(DMA_LENGTH_REG_OFFSET));
+    printk(KERN_INFO "ham_drv: dma reg control          %u\n", ioread32(DMA_CONTROL_REG_OFFSET));
+}
+
+static void ham_drv_dma_print_data(void)
+{
+    for (int i = 0; i < DMA_BUFFER_SIZE; ++i) {
+        printk(KERN_INFO "ham_drv: dma data %d: %u\n", i, (*((unsigned char *)(buffer + i))));
+    }
+}
 
 static long ham_drv_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 {
@@ -47,8 +76,9 @@ static irqreturn_t ham_drv_interrupt(int irq, void *dev_id)
     if (irq != INTERRUPT_NUM) {
         return IRQ_NONE;
     }
-    adc_value = ioread32(GET_ADC_OFFSET);
-    printk(KERN_INFO "ham_drv: adc %ld\n", adc_value);
+    iowrite32(130, DMA_CONTROL_REG_OFFSET);
+    ham_drv_dma_print_registers();
+    iowrite32(0, DMA_STATUS_REG_OFFSET);
     return IRQ_HANDLED;
 }
 
@@ -95,9 +125,28 @@ static int __init ham_drv_init(void)
     }
     printk(KERN_INFO "ham_drv: request irq %d\n", INTERRUPT_NUM);
 
+    buffer = kmalloc(DMA_BUFFER_SIZE, GFP_DMA);
+    if (buffer == NULL) {
+        goto fail_kmalloc_buffer;
+    }
+
+    ham_drv_dma_print_registers();
+    iowrite32(146, DMA_CONTROL_REG_OFFSET);
+
+    ham_drv_dma_print_registers();
+    iowrite32(buffer, DMA_WRITEADDRESS_REG_OFFSET);
+    iowrite32(DMA_BUFFER_SIZE, DMA_LENGTH_REG_OFFSET);
+    iowrite32(0, DMA_READADDRESS_REG_OFFSET);
+
+    ham_drv_dma_print_registers();
+    iowrite32(154, DMA_CONTROL_REG_OFFSET);
+
+    ham_drv_dma_print_registers();
     printk(KERN_INFO "ham_drv: successfully initialized\n");
     return 0;
 
+fail_kmalloc_buffer:
+    free_irq(INTERRUPT_NUM, 0);
 fail_request_irq:
     iounmap(ham_drv_mem);
 fail_ioremap:
@@ -116,6 +165,8 @@ fail_register_chrdev:
 
 static void __exit ham_drv_exit(void)
 {
+    ham_drv_dma_print_data();
+    kfree(buffer);
     free_irq(INTERRUPT_NUM, 0);
     iounmap(ham_drv_mem);
     release_mem_region(UINPUT_BASE, UINPUT_SIZE);
